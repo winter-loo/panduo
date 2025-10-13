85199a5665160a5933a49ac99aaeca568ebdf8e8
winter.loo <winterloo@126.com>
2025-10-13 20:32:21 +0800

    use rounded corners for piano key

--- paigo/src/lib/components/piano-key/piano-key.svelte
+++ paigo/src/lib/components/piano-key/piano-key.svelte
@@ -13,22 +13,27 @@
   to include shadow spacing
 -->
 <div class="piano-key relative py-2 pl-1" data-name={name} data-octave={octave}>
+  <!-- lesson leart: on active, do not remove the second shadow even it has the same value as the first one. -->
+  <!-- Instead, change only what should be changed -->
   <button
     class="white-key ease z-1
-    flex h-48 w-18 items-end justify-center bg-white text-[var(--note-default)]
+    flex h-48 w-18 items-end justify-center rounded-sm bg-white
+    text-[var(--note-default)]
     shadow-[0_0_0_var(--spacing)_var(--border),0_var(--spacing)_0_var(--spacing)_var(--border)]
     transition-all
     duration-200
     hover:bg-[var(--key-hover)]
     active:translate-y-1
-    active:bg-[var(--key-active)]"
+    active:bg-[var(--key-active)]
+    active:shadow-[0_0_0_var(--spacing)_var(--border),0_0_0_var(--spacing)_var(--border)]"
   >
     {name + octave.toString()}
   </button>
   {#if name != 'E' && name != 'B' && !hideBlack}
     <button
       class="black-key ease absolute top-0 left-11 z-2 flex h-24
-      w-13 items-end justify-center bg-[var(--note-default-500)]
+      w-13 items-end justify-center rounded-sm
+      bg-[var(--note-default-500)]
       text-white
       shadow-[0_calc(var(--spacing)*2)_0_var(--note-default-100)] transition-all
       duration-200 hover:bg-[var(--note-default-300)] active:top-1
