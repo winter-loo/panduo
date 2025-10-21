b8b85aaf747244c298c2e3bdedaede71f177ca51
winter.loo <adjkldd@126.com>
2025-10-22 01:17:08 +0800

    tweaking navbar style

--- paigo/src/lib/components/ui/navigation-menu/navigation-menu.svelte
+++ paigo/src/lib/components/ui/navigation-menu/navigation-menu.svelte
@@ -19,7 +19,7 @@
   data-slot="navigation-menu"
   data-viewport={viewport}
   class={cn(
-    'group/navigation-menu relative flex max-w-max flex-1 items-center justify-center',
+    'group/navigation-menu relative flex bg-[var(--app-lightest)] w-full flex-1 items-center justify-center',
     className,
   )}
   {...restProps}
