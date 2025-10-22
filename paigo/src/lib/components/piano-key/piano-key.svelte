f63486278b196e5540cbad90012f028e96f903c7
winter.loo <adjkldd@126.com>
2025-10-23 04:04:05 +0800

    refine style of piano key component

--- paigo/src/lib/components/piano-key/piano-key.svelte
+++ paigo/src/lib/components/piano-key/piano-key.svelte
@@ -6,6 +6,7 @@
   export type PianoKeyProps = {
     name: PianoKeyName;
     octave: number;
+    highlight?: boolean;
     hideBlack?: boolean;
     onnoteon?: (name: PianoKeyFullName) => void;
     onnoteoff?: (name: PianoKeyFullName) => void;
@@ -15,6 +16,7 @@
   let {
     name,
     octave,
+    highlight = false,
     hideBlack = false,
     onnoteon,
     onnoteoff,
@@ -69,38 +71,78 @@
     blackActive = false;
     onnoteoff?.(name);
   }
+
+  // 'data-[active=true]:bg-[var(--note-a)] data-[active=true]:shadow-[inset_0_calc(var(--spacing)*-1)_0_var(--note-a)]'
+  const keyHighlightStyle: Record<string, string[]> = {
+    A: [
+      'data-[active=true]:ring-[var(--note-a)]/40 data-[active=true]:after:bg-[var(--note-a)]/20',
+      'data-[active=true]:bg-[var(--note-a)] data-[active=true]:shadow-[var(--note-a)]',
+    ],
+    B: [
+      'data-[active=true]:ring-[var(--note-b)]/40 data-[active=true]:after:bg-[var(--note-b)]/20',
+      'data-[active=true]:bg-[var(--note-b)] data-[active=true]:shadow-[var(--note-b)]',
+    ],
+    C: [
+      'data-[active=true]:ring-[var(--note-c)]/40 data-[active=true]:after:bg-[var(--note-c)]/20',
+      'data-[active=true]:bg-[var(--note-c)] data-[active=true]:shadow-[var(--note-c)]',
+    ],
+    D: [
+      'data-[active=true]:ring-[var(--note-d)]/40 data-[active=true]:after:bg-[var(--note-d)]/20',
+      'data-[active=true]:bg-[var(--note-d)] data-[active=true]:shadow-[var(--note-d)]',
+    ],
+    E: [
+      'data-[active=true]:ring-[var(--note-e)]/40 data-[active=true]:after:bg-[var(--note-e)]/20',
+      'data-[active=true]:bg-[var(--note-e)] data-[active=true]:shadow-[var(--note-e)]',
+    ],
+    F: [
+      'data-[active=true]:ring-[var(--note-f)]/40 data-[active=true]:after:bg-[var(--note-f)]/20',
+      'data-[active=true]:bg-[var(--note-f)] data-[active=true]:shadow-[var(--note-f)]',
+    ],
+    G: [
+      'data-[active=true]:ring-[var(--note-g)]/40 data-[active=true]:after:bg-[var(--note-g)]/20',
+      'data-[active=true]:bg-[var(--note-g)] data-[active=true]:shadow-[var(--note-g)]',
+    ],
+  };
 </script>
 
 <!--
   * use 'my-2 ml-1 last:mr-1' to have exactly width taken visually by this piano key, i.e.,
     to include shadow spacing
-  * the CSS custom property keeps the black key decoupled from the white key's active height change
-  * implementation notes: use shadown and inset shadow and height transition to create a
-    slide up/down effect on click.
-    Pros:
-      - easy to handle spacing bewteen piano keys
-      - non clickable on the left and right shadows
-    Cons:
-      - inset shadow is clickable
-      - need set `translate-y-0` on an inner child
+  * use ':after' pseudo element to add overlay while hovering or being active
+  * use 'z-2' while being active to highlight shadow and 'z-10' to put black keys
+    on upper top layer
 -->
 <div
   class="piano-key relative mt-2 mb-1 ml-1 inline-flex h-[var(--white-key-height)] items-end last:mr-1"
   data-name={name}
   data-octave={octave}
 >
-  <!-- lesson leart: on active, do not remove the second shadow even it has the same value as the first one. -->
-  <!-- Instead, change only what should be changed -->
+  <!-- shadow-[0_0_0_var(--spacing)_var(--border)]  -->
   <button
-    class={`white-key group z-1 flex
-    h-[var(--white-key-height)] w-18 items-end justify-center rounded-sm bg-white text-[var(--note-black)]
-    shadow-[0_0_0_var(--spacing)_var(--border),inset_0_calc(var(--spacing)*-1)_0_0_var(--border)] transition-all duration-200
+    class={`white-key group relative z-1 flex
+    h-[calc(var(--white-key-height))] w-18 items-end justify-center rounded-sm bg-white text-[var(--note-black)]
+    ring-4 ring-[var(--border)]
+    transition-all duration-200
     ease-out
-    hover:bg-[var(--note-black-50)]
+    after:pointer-events-none
+    after:absolute
+    after:inset-0
+    after:-z-1
+    after:rounded-sm
+    after:border-b-4
+    after:border-[var(--border)]
+    after:bg-white
+    after:select-none
+    hover:after:bg-[var(--note-black-100)]/20
     focus:outline-none
+    data-[active=true]:z-2
     data-[active=true]:h-[var(--white-key-height-active)]
-    data-[active=true]:bg-[var(--note-black-50)]
-    data-[active=true]:shadow-[0_0_0_var(--spacing)_var(--border),inset_0_0_0_var(--border)]
+    data-[active=true]:after:border-0
+    ${
+      highlight
+        ? keyHighlightStyle[name][0]
+        : 'data-[active=true]:after:bg-[var(--note-black-100)]/40'
+    }
     `}
     data-active={whiteActive}
     onpointerdown={() => startWhitePress({ name, octave, sharp: false })}
@@ -115,15 +157,18 @@
   </button>
   {#if name != 'E' && name != 'B' && !hideBlack}
     <button
-      class={`black-key group absolute -top-2 left-11 z-2 flex h-[var(--black-key-height)] w-13 items-end justify-center
+      class={`black-key group absolute -top-2 left-11 z-10 flex h-[var(--black-key-height)] w-13 items-end justify-center
       rounded-sm bg-[var(--note-black)] text-white shadow-[inset_0_calc(var(--spacing)*-2)_0_var(--note-black-900)]
       transition-all
       duration-200
       ease-out
       hover:bg-[var(--note-black-600)] focus:outline-none
       data-[active=true]:-top-1
-      data-[active=true]:bg-[var(--note-black-700)]
-      data-[active=true]:shadow-[inset_0_calc(var(--spacing)*-1)_0_var(--note-black-900)]
+      ${
+        highlight
+          ? keyHighlightStyle[name][1]
+          : 'data-[active=true]:bg-[var(--note-black-900)] data-[active=true]:shadow-[var(--note-black-900)]'
+      }
       `}
       data-active={blackActive}
       onpointerdown={() => startBlackPress({ name, octave, sharp: true })}
