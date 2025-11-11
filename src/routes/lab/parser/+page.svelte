36c025c465333c4091b5dd116f4af6261427f1c7
winter.loo <adjkldd@126.com>
2025-11-11 08:34:15 +0800

    new ohm parser experimenting page

--- src/routes/lab/parser/+page.svelte
+++ src/routes/lab/parser/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+import { onMount } from 'svelte';
   import * as ohm from 'ohm-js';
+  import * as InputGroup from "$lib/components/ui/input-group/index.js";
+  import { Save as SaveIcon, Copy as CopyIcon } from '@lucide/svelte';
 
-  const abcNotation = ohm.grammar(String.raw`
+  let abcNotation = ohm.grammar(String.raw`
   AbcNotation {
       File = FileHeader (EmptyLines TuneBook)
       TuneBook = listOf<Tune, EmptyLines>
@@ -107,238 +110,209 @@
   }
 `;
 
-`
-Abc {
-    NoteSeq =
-    	NoteSeq NoteSeq  --concat
-        |  note ">" note --broken
-        |  note #("-") note --tie
-        |  "(" NoteSeq ")" --slur
-        | "{" NoteSeq "}" --grace
-        | "("#(digit) NoteSeq  --nplet
-    	| note
+  let currentGrammar = $state('');
+  let matchingText = $state('');
+  let traceOutput = $state('');
+  let failureMessage = $state('');
 
-    note = "A".."G" | "a".."g"
-  }
-`;
+  onMount(() => {
+    currentGrammar = String.raw`
+      Abc {
+          NoteSeq =
+            | NoteSeq NoteSeq  --concat
+            | "(" NoteSeq ")" --slur
+            | "{" NoteSeq "}" --grace
+            | "("#(digit) NoteSeq  --nplet
+            | NoteConstruct BinaryNoteOp NoteConstruct --binary
+            | NoteConstruct
 
-`
-Abc  {
-    NoteSeq =
-    	NoteSeq NoteSeq  --concat
-        |  note ">" note --broken
-        |  note #("-") note --tie
-        |  "(" NoteSeq ")" --slur
-        | "{" NoteSeq "}" --grace
-        | "("#(digit) NoteSeq  --nplet
-        | "[" #(NoteConstruct)+ "]" --chord
-        | NoteConstruct
+          BinaryNoteOp =
+            | BinaryNoteOp BinaryNoteOp  --multi
+            | ">" --broken
+            | "-" --tie
 
+          NoteConstruct  = ChordAnnotation? AnnotationList? CoreNote
 
-    NoteConstruct  = ChordAnnotation? AnnotationList? CoreNote
+          ChordAnnotation = "\"" ChordAnnoText "\""
+          ChordAnnoText = note
+          ChordSymbol = "Am"
 
-    ChordAnnotation = "\"" ChordAnnoText "\""
-    ChordAnnoText = note
-    ChordSymbol = "Am"
+          PairingAnnotationNote = CoreNoteWithStartAnnotation NoteSeq* CoreNoteWithEndAnnotation
 
-    PairingAnnotationNote = CoreNoteWithStartAnnotation NoteSeq* CoreNoteWithEndAnnotation
+          CoreNoteWithStartAnnotation = ChordAnnotation? AnnotationStart CoreNote
+          CoreNoteWithEndAnnotation = ChordAnnotation? AnnotationEnd CoreNote
 
-    CoreNoteWithStartAnnotation = ("\"" ChordSymbol "\"")? AnnotationStart CoreNote
-    CoreNoteWithEndAnnotation = ("\"" ChordSymbol "\"")? AnnotationEnd CoreNote
+          AnnotationStart =
+            | "!trill(!"
+            | "!crescendo(!"
+            | "!<(!"
+            | "!diminuendo(!"
+            | "!>(!"
 
-    CoreNote = #(accidental)? #(note) #(octave)? #(noteLen)?
+          AnnotationEnd =
+            | "!trill)!"
+            | "!crescendo)!"
+            | "!<)!"
+            | "!diminuendo)!"
+            | "!>)!"
 
-    AnnotationStart =
-      | "!trill(!"
-      | "!crescendo(!"
-      | "!<(!"
-      | "!diminuendo(!"
-      | "!>(!"
+          CoreNote = #(accidental)? #(note) #(octave)? #(noteLen)?
 
-    AnnotationEnd =
-      | "!trill)!"
-      | "!crescendo)!"
-      | "!<)!"
-      | "!diminuendo)!"
-      | "!>)!"
+          AnnotationList =
+            | AnnotationList Annotation --list
+            | Annotation --single
+          Annotation =
+            | "\"" AnnoText "\"" --anno
+            | Decoration --deco
+          AnnoText = (~EOL any)*
 
-     AnnotationList =
-     	| AnnotationList Annotation --list
-     	| Annotation --single
-     Annotation =
-      | "\"" AnnoText "\"" --anno
-      | Decoration --deco
-    AnnoText = (~EOL any)*
+          Decoration =
+              | "."
+              | "~"
+              | "H"
+              | "L"
+              | "M"
+              | "O"
+              | "P"
+              | "S"
+              | "T"
+              | "u"
+              | "v"
+              | "!trill!"
+              | "!lowermordent!"
+              | "!uppermordent!"
+              | "!mordent!"
+              | "!pralltriller!"
+              | "!roll!"
+              | "!turn!"
+              | "!turnx!"
+              | "!invertedturn!"
+              | "!invertedturnx!"
+              | "!arpeggio!"
+              | "!>!"
+              | "!accent!"
+              | "!emphasis!"
+              | "!fermata!"
+              | "!invertedfermata!"
+              | "!tenuto!"
+              | "!0!"
+              | "!1!"
+              | "!2!"
+              | "!3!"
+              | "!4!"
+              | "!5!"
+              | "!+!"
+              | "!plus!"
+              | "!snap!"
+              | "!slide!"
+              | "!wedge!"
+              | "!upbow!"
+              | "!downbow!"
+              | "!open!"
+              | "!thumb!"
+              | "!breath!"
+              | "!pppp!"
+              | "!ppp!"
+              | "!pp!"
+              | "!p!"
+              | "!mp!
+              | "!mf!"
+              | "!f!"
+              | "!ff!"
+              | "!fff!"
+              | "!ffff!"
+              | "!sfz!"
+              | "!segno!"
+              | "!coda!"
+              | "!D.S.!"
+              | "!D.C.!"
+              | "!dacoda!"
+              | "!dacapo!"
+              | "!fine!"
+              | "!shortphrase!"
+              | "!mediumphrase!"
+              | "!longphrase!"
 
-    Decoration =
-      | "."
-      | "~"
-      | "H"
-      | "L"
-      | "M"
-      | "O"
-      | "P"
-      | "S"
-      | "T"
-      | "u"
-      | "v"
-      | "!trill!"
-      | "!lowermordent!"
-      | "!uppermordent!"
-      | "!mordent!"
-      | "!pralltriller!"
-      | "!roll!"
-      | "!turn!"
-      | "!turnx!"
-      | "!invertedturn!"
-      | "!invertedturnx!"
-      | "!arpeggio!"
-      | "!>!"
-      | "!accent!"
-      | "!emphasis!"
-      | "!fermata!"
-      | "!invertedfermata!"
-      | "!tenuto!"
-      | "!0!"
-      | "!1!"
-      | "!2!"
-      | "!3!"
-      | "!4!"
-      | "!5!"
-      | "!+!"
-      | "!plus!"
-      | "!snap!"
-      | "!slide!"
-      | "!wedge!"
-      | "!upbow!"
-      | "!downbow!"
-      | "!open!"
-      | "!thumb!"
-      | "!breath!"
-      | "!pppp!"
-      | "!ppp!"
-      | "!pp!"
-      | "!p!"
-      | "!mp!
-      | "!mf!"
-      | "!f!"
-      | "!ff!"
-      | "!fff!"
-      | "!ffff!"
-      | "!sfz!"
-      | "!segno!"
-      | "!coda!"
-      | "!D.S.!"
-      | "!D.C.!"
-      | "!dacoda!"
-      | "!dacapo!"
-      | "!fine!"
-      | "!shortphrase!"
-      | "!mediumphrase!"
-      | "!longphrase!"
+          accidental = "^"+ | "_"+ | "="
+          note = "A".."G" | "a".."g"
+          octave = ","+ | "'"+
+          noteLen = digit
 
-    accidental = "^"+ | "_"+ | "="
-    note = "A".."G" | "a".."g"
-    octave = ","+ | "'"+
-    noteLen = digit
-  }
-`;
+          InlineInfoFieldList = InlineInfoField*
+          InlineInfoField = "["  Key ":" InlineFieldValue "]"
+          InlineFieldValue     = (~(EOL | "]" | "[") any)*
 
+          InBodyBlockInfoFieldList = EOL listOf<InfoField, EOL> EOL
+          InfoField = Key ":" Value
+          Key = letter
+          Value = (~EOL any)*
+          EmptyLines = EOL+
+          EOL = "\r\n" | "\n" | "\r"
+          note = "A".."G" | "a".."g"
+          space := " " | "\t"
+      }
+      `;
+      matchingText = "\nK:A\n";
+  });
 
-  `
-Abc  {
-    NoteSeq =
-    	NoteSeq NoteSeq  --concat
-        |  NoteConstruct BinaryNoteOp NoteConstruct --binary
-        |  "(" NoteSeq ")" --slur
-        |  "{" NoteSeq "}" --grace
-        | UnaryOp NoteSeq  --nplet
-    	| NoteConstruct
-
-       UnaryOp = "(" #(digit)
-       BinaryNoteOp = BinaryNoteOp BinaryNoteOp  --multi
-       		| ">"
-            | "-"
-
-
-     NoteConstruct  = ("\"" ChordSymbol "\"")? AnnotationList?  CoreNote
-
-     CoreNote = #(accidental)? #(note) #(octave)? #(noteLen)? --single
-     		| "[" NoteConstruct+ "]" --chord
-
-
-     ChordSymbol = "Am"
-     AnnotationList =
-     	| AnnotationList Annotation --list
-     	| Annotation --single
-     Annotation =
-     	"\""  AnnoText "\"" --anno
-     	| "!0!" --deco
-        | "!f!"
-
-     AnnoText =
-     	| "^I"
-    accidental = "^"+ | "_"+ | "="
-    note = "A".."G" | "a".."g"
-    octave = ","+ | "'"+
-    noteLen = digit
-  }
-`;
-
-`
-Abc {
-  NoteSeq =
-    | NoteConstruct BinaryNoteOp NoteConstruct --binary
-    | NoteConstruct
-
-  BinaryNoteOp =
-    | BinaryNoteOp BinaryNoteOp  --multi
-    | ">"
-    | "-"
-
-
-  NoteConstruct = CoreNote
-
-  CoreNote = note
-
-  note = "A".."G" | "a".."g"
-}
-`;
-
-
-`
-Abc {
-    InlineInfoFieldList = InlineInfoField*
-    InlineInfoField = "["  Key ":" InlineFieldValue "]"
-    Key       = letter
-    InlineFieldValue     = (~(EOL | "]" | "[") any)*
-    EOL = "\r\n" | "\n" | "\r"
-    space := " " | "\t" | end
-}
-`;
-
-
-  `
-Abc {
-	BlockInfoField = InfoField EOL
-
-    InfoField = Key ":" Value
-    Key = letter
-    Value = (~EOL any)*
-    EmptyLines = EOL+
-    EOL = "\r\n" | "\n" | "\r"
-    space := " " | "\t" | end
-}
-
-`;
-  console.log(abcNotation);
-
-  const mr = abcNotation.match(String.raw`
-t:title
-
-K:c
-A,2 C/4 c3/4 | c"
-`);
-  console.log(mr);
-  console.log('matched: ', mr.succeeded());
+  $effect(() => {
+    let abcNotation = ohm.grammar(currentGrammar);
+    const mr = abcNotation.match(matchingText);
+    if (mr.failed()) {
+      failureMessage = mr.message!.replaceAll(" ", "&nbsp;").replaceAll("\n", "<br />");
+    } else {
+      failureMessage = '';
+    }
+    let to = abcNotation.trace(matchingText).toString();
+    to = to.replaceAll(" ", "&nbsp;");
+    to = to.replaceAll("\n", "<br />");
+    traceOutput = to;
+    });
 </script>
+
+<div class="flex justify-between w-screen max-h-screen">
+  <div class="flex flex-col w-full max-h-full px-8 py-4">
+    <div class="grid w-full gap-4">
+     <InputGroup.Root>
+      <InputGroup.Addon align="block-start" class="border-b">
+       <InputGroup.Button class="ml-auto" size="icon-xs">
+        <SaveIcon />
+       </InputGroup.Button>
+       <InputGroup.Button variant="ghost" size="icon-xs">
+        <CopyIcon />
+       </InputGroup.Button>
+      </InputGroup.Addon>
+      <InputGroup.Textarea
+       placeholder="ohm grammar: https://ohmjs.org/docs/syntax-reference"
+       class="min-h-[200px]"
+        bind:value={currentGrammar}
+      />
+     </InputGroup.Root>
+    </div>
+    <div class="grid w-full gap-4">
+     <InputGroup.Root>
+      <InputGroup.Addon align="block-start" class="border-b">
+       <InputGroup.Button class="ml-auto" size="icon-xs">
+        <SaveIcon />
+       </InputGroup.Button>
+       <InputGroup.Button variant="ghost" size="icon-xs">
+        <CopyIcon />
+       </InputGroup.Button>
+      </InputGroup.Addon>
+      <InputGroup.Textarea
+       placeholder="ohm grammar: https://ohmjs.org/docs/syntax-reference"
+       class="min-h-[200px]"
+        bind:value={matchingText}
+      />
+     </InputGroup.Root>
+    </div>
+  </div>
+  <div class="flex flex-col max-h-full p-8">
+    {#if failureMessage.length > 0}
+      <p class="text-red-500 text-lg">{@html failureMessage}</p>
+      <hr class="my-2 "/>
+    {:else}
+      <p class="max-h-fit overflow-scroll">{@html traceOutput}</p>
+    {/if}
+  </div>
+</div>
