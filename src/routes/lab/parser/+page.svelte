ef4d9c617a715267c81172e721b862602ae32e37
winter.loo <winterloo@126.com>
2025-11-13 13:31:14 +0800

    update match result when text in editor changes

--- src/routes/lab/parser/+page.svelte
+++ src/routes/lab/parser/+page.svelte
@@ -3,7 +3,9 @@
   import abcNotation from './abc.ohm?raw';
   import test1 from './testdata/1.txt?raw';
   import { onMount } from 'svelte';
-  import {basicSetup, EditorView} from 'codemirror';
+  import { basicSetup } from 'codemirror';
+  import { EditorState } from '@codemirror/state';
+  import { EditorView } from '@codemirror/view';
 
   let currentGrammarText = $state(abcNotation);
   let testText = $state('X:1\nK:D\n[CEG]->[CEG]');
@@ -30,21 +32,56 @@
     }
   });
 
+  function debounce<F extends (...args: any[]) => void>(fn: F, delay: number) {
+    let timer: number | undefined;
+    return (...args: Parameters<F>) => {
+      if (timer) clearTimeout(timer);
+      timer = window.setTimeout(() => fn(...args), delay);
+    };
+  }
+
+  const onDebouncedUpdate = debounce((view: EditorView) => {
+    // console.log("Debounced update:", view.state.doc.toString());
+    let updatedText = view.state.doc.toString();
+    if (view == grammarView) {
+      currentGrammarText = updatedText;
+    } else if (view == testView) {
+      testText = updatedText;
+    }
+  }, 500); // 500ms delay after last input
+
   let grammarRef = $state();
   let testRef = $state();
+  let grammarView: EditorView;
+  let testView: EditorView;
   onMount(() => {
-    // TODO: how to trigger an change event after some delay
-    const grammarView = new EditorView({
+    const updateListener = EditorView.updateListener.of((update) => {
+      if (update.docChanged) {
+        onDebouncedUpdate(update.view);
+      }
+    });
+    const grammarEditingState = EditorState.create({
       doc: currentGrammarText,
+      extensions: [basicSetup, updateListener],
+    });
+    grammarView = new EditorView({
+      state: grammarEditingState,
       parent: grammarRef! as Element,
-      extensions: [basicSetup]
     });
-    const testView = new EditorView({
+    const textEditingState = EditorState.create({
       doc: testText,
-      parent: testRef! as Element,
-      extensions: [basicSetup]
+      extensions: [basicSetup, updateListener],
     });
-  })
+    testView = new EditorView({
+      state: textEditingState,
+      parent: testRef! as Element,
+    });
+
+    return () => {
+      grammarView.destroy();
+      testView.destroy();
+    };
+  });
 
   let minLeft = $state(false);
   let minRight = $state(false);
@@ -56,31 +93,38 @@
     minRight = !minRight;
   }
 
+  let toTop = $state(false);
+  function toggleExpandToTop() {
+    toTop = !toTop;
+  }
+
   let showTrace = $state(false);
   $effect(() => {
     if (showTrace && abcGrammar) {
-      if (testText.length < 400)
+      if (testText.length < 100)
         traceOutput = abcGrammar.trace(testText).toString();
       else
-        traceOutput = "input text is too long enough to trace";
+        traceOutput = "input text is too long to trace, max: 100";
     }
   });
 </script>
 
 <div class="flex flex-col justify-center items-center w-[calc(100vw-20px)] h-screen">
-  <div class="flex w-full h-3/4 px-1 py-4">
-    <div bind:this={grammarRef} class={`grammar ${minRight ? 'w-full' : minLeft ? 'w-0' : 'w-1/2'} h-full flex flex-col overflow-auto transation-width ease-out duration-150`}></div>
-    <div class="flex flex-col handle w-2 h-full bg-[var(--app-color-100)]">
-      <button class="left w-full h-full text-[8px] text-center hover:bg-[var(--app-color-150)]" onclick={toggleExpandToLeft} aria-label="expand left">
-        {minLeft ? '>' : '<'}
+  <div class={`flex w-full ${toTop ? 'h-1/4' : 'h-3/4'} px-1 pt-4 transition-height ease-out duration-150`}>
+    <div bind:this={grammarRef} class={`grammar ${minRight ? 'w-full' : minLeft ? 'w-0' : 'w-1/2'} h-full flex flex-col overflow-auto transition-width ease-out duration-150`}></div>
+    <div class="flex flex-col handle w-2 h-full bg-border">
+      <button class="left w-full h-full text-[8px] flex items-center justify-center hover:bg-[var(--app-color-150)] hover:text-base" onclick={toggleExpandToLeft} aria-label="expand left">
+        {minLeft ? '⇒' : '⇐'}
       </button>
-      <button class="right w-full h-full text-[8px] text-center hover:bg-[var(--app-color-150)]" onclick={toggleExpandToRight} aria-label="expand right">
-        {minRight ? '<' : '>'}
+      <button class="right w-full h-full text-[8px] flex items-center justify-center hover:bg-[var(--app-color-150)] hover:text-base" onclick={toggleExpandToRight} aria-label="expand right">
+        {minRight ? '⇐' : '⇒'}
       </button>
     </div>
-    <div bind:this={testRef} class={`test ${minLeft ? 'w-full' : minRight ? 'w-0' : 'w-1/2'} h-full flex flex-col overflow-auto transation-width ease-out duration-150`}></div>
+    <div bind:this={testRef} class={`test ${minLeft ? 'w-full' : minRight ? 'w-0' : 'w-1/2'} h-full flex flex-col overflow-auto transition-width ease-out duration-150`}></div>
   </div>
-  <div class="flex w-full flex-col h-1/4 p-8 border-t-4">
+  <button class="w-full h-2 bg-border text-[8px] flex items-center justify-center font-bold
+    hover:bg-[var(--app-color-150)] hover:text-base" onclick={toggleExpandToTop} aria-label="expand top">↕</button>
+  <div class={`flex w-full flex-col ${toTop ? 'h-3/4' : 'h-1/4'} p-8 transition-height ease-out duration-150`}>
     {#if resultMessage.length > 0}
       <pre class="text-red-500 text-base text-wrap">{resultMessage}</pre>
       <button onclick={() => (showTrace = !showTrace)}>show trace</button>
